da2013b4a99ebe77db7c1376e5d7e989c6f20df4
Almas <almas1610@gmail.com>
2022-05-21 04:25:43 +0600

    developing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC, ReactComponentElement, useState} from 'react';
 import './App.css';
 
+export interface Todos {
+    title: string,
+    checked?: boolean,
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const text: string = 'Enter your text';
+
+    const todo: Todos[] = [
+        {title: 'Learn Redux Toolkit', checked: true},
+        {title: 'Learn NGRX', checked: false},
+        {title: 'Learn NestJS', checked: false}
+    ]
+
+    return (
+        <div className="App">
+            <h1>Hello World!</h1>
+            <TodoList text={text}/>
+            <div>
+                {
+                    todo.map((temp) => (
+                        <List title={temp.title} checked={temp.checked} key={temp.title}/>
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
+
+export function TodoList(props: any) {
+    const [text, setText] = useState<any>('');
+
+    return (
+        <div className={props.text}>
+            <h3>{props.text}</h3>
+            <h3>{text}</h3>
+            <input value={text} onChange={(e) => setText(e.target.value)} type="text"></input>
+        </div>
+    );
+}
+
+export function List(props: any): JSX.Element {
+    return (
+        <div>
+            {props.title}
+        </div>
+    )
 }
 
 export default App;
