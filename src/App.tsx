29e4ff9a0f388ecfacc915f08e7710e0c1375b23
Almas <almas1610@gmail.com>
2022-05-22 21:26:05 +0600

    test, repeat, learn etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, {FC, ReactComponentElement, useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { makeStyles } from "@mui/material";
 
 export interface Todos {
     title: string,
@@ -9,16 +10,18 @@ export interface Todos {
 function App() {
 
     const text: string = 'Enter your text';
+    const paragraph: string = 'Lorem ipsum'
 
     const todo: Todos[] = [
         {title: 'Learn Redux Toolkit', checked: true},
         {title: 'Learn NGRX', checked: false},
         {title: 'Learn NestJS', checked: false}
-    ]
+    ];
 
     return (
         <div className="App">
             <h1>Hello World!</h1>
+            <Todo paragraph={paragraph}/>
             <TodoList text={text}/>
             <div>
                 {
@@ -31,11 +34,32 @@ function App() {
     );
 }
 
-export function TodoList(props: any) {
-    const [text, setText] = useState<any>('');
+export const Todo: React.FC<any> = ({paragraph}) => {
+    return (
+        <div>
+            {paragraph}
+        </div>
+    )
+}
+
+export const TodoList: React.FC<any> = (props) => {
+    const [text, setText] = useState<string>('Hi Almas!');
+    const [users, setUsers] = useState<any>([]);
+    const [toggle, setToggle] = useState<boolean>(false);
+
+    React.useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(data => data.json())
+            .then(users => setUsers(users));
+    },[]);
 
     return (
         <div className={props.text}>
+            {users.map((user: any) => (
+                <ul>
+                    <li style={{listStyle: 'none'}} key={user.name}>{user.name} ------------- {user.email}</li>
+                </ul>
+            ))}
             <h3>{props.text}</h3>
             <h3>{text}</h3>
             <input value={text} onChange={(e) => setText(e.target.value)} type="text"></input>
